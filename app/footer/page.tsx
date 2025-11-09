10cf7d224745a3e8ca7d54ef1fd253f6f778595c
pshemssa <s.patience@alustudent.com>
2025-11-09 11:52:59 +0200

    added footer page for visualisation  advantage

---
+++ app/footer/page.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center py-4">
+      <p>© 2025 TaskFlow. All rights reserved.</p>
+    </footer>
+  );
+}
